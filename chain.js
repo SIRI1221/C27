cc9be2c940556fcd193306e88993ff3dbe54e3bb
SIRI1221 <75670297+SIRI1221@users.noreply.github.com>
2021-05-17 12:50:32 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,23 @@
+class Chain{
+constructor(bodyA,bodyB){
+    var options={
+        bodyA:bodyA,
+        bodyB:bodyB,
+        length:10,
+        stiffness:0.04
+    }
+    
+    
+    this.rope=Constraint.create(options);
+    World.add(world,this.rope);
+
+}
+display()
+{   
+  var pointA=this.rope.bodyA.position;
+  var pointB=this.rope.bodyB.position;  
+ strokeWeight(4);
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+}
+}
